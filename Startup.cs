d5a4b704ddff436977542d3a7e4e4dcd8b6023d2
Mark Huntjens <mjjhuntjens@gmail.com>
2020-07-11 00:17:49 +0200

    migration for database, connection not working

--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WorldCities.Data;
 
 namespace WorldCities
 {
@@ -26,6 +28,16 @@ namespace WorldCities
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            // Add EntityFramework support for SqlServer.
+            //services.AddEntityFrameworkSqlServer();
+
+            // Add ApplicationDbContext.
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")
+                    )
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
